341b35275197836fe04582d70126ed0cf6854f8b
Thomas Lotsander <31844571+ThomasLotsander@users.noreply.github.com>
2017-09-12 15:35:25 +0200

    Tog bort enkelt chiffer filen

--- Sudoku/Sudoku/Program.cs
+++ Sudoku/Sudoku/Program.cs
@@ -4,21 +4,19 @@ using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApp2
+namespace Sudoku
 {
     class Program
     {
         static void Main(string[] args)
         {
             // Skickar in en sträng med alla nummer som ska fylla Sudokon
-            var Sudoko = new Sudoku("003020600900305001001806400008102900700000008006708200002609500800203009005010300");
+            var Sudoku = new Sudoku("003020600900305001001806400008102900700000008006708200002609500800203009005010300");
 
             // Skriver ut brädet i consolen 
-            Sudoko.SkrivUtSudokoBräde();
-            Sudoko.SkrivUtAllaBoxar();
+            Sudoku.SkrivUtSudokoBräde();
+            Sudoku.SkrivUtAllaBoxar();
            // Sudoko.LösSudokun();
-
-            System.Console.WriteLine("Hello grupp E");
 
             Console.ReadLine();
         }
