aee89309d40b86281c398dc1b4e0b699d5db377b
Dagdelius <31845365+Dagdelius@users.noreply.github.com>
2017-09-13 14:42:07 +0200

    Optimera kod

--- Sudoku/Sudoku/Sudoku.cs
+++ Sudoku/Sudoku/Sudoku.cs
@@ -1,262 +1,98 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace Sudoku
+namespace TestSudoku
 {
     class Sudoku
     {
-        // ====
-        // Fält
-        // ====
-        private string sudokoNummer;
+        private int[,] board = new int[9, 9];
+        private string[] row = new string[9];
+        private string[] col = new string[9];
+        private string[] box = new string[9];
+        private string checknr;
 
-        // String med nummer som ska finnas med
-        private string[] nummerEttTillNio = new string[]{ "1", "2", "3", "4", "5", "6", "7", "8", "9" };
-
-        // Lista som sparar alla nummer i sudokun som inte är 0
-        List<char> nummerSomFinns = new List<char>();
-
-        // Skapar alla 9o boxar som ska inehålla "nummerEttTillNio"
-        private char[] topLeftBox = new char[9];
-        private char[] topMideBox = new char[9];
-        private char[] topRightBox = new char[9];
-        private char[] middleLeftBox = new char[9];
-        private char[] middleMidBox = new char[9];
-        private char[] middleRightBox = new char[9];
-        private char[] bottomLeftBox = new char[9];
-        private char[] bottomMidBox = new char[9];
-        private char[] bottomRightBox = new char[9];
-
-        // individuell counter för varje box
-        // Går säkert att göra på ett snyggare sätt
-        private int box1 = 0;
-        private int box2 = 0;
-        private int box3 = 0;
-        private int box4 = 0;
-        private int box5 = 0;
-        private int box6 = 0;
-        private int box7 = 0;
-        private int box8 = 0;
-        private int box9 = 0;
-
-
-        // ===========
-        //Konstruktors
-        //============
-
-        // Konstruktor som sätter värdet på sudokoNummer
-        public Sudoku(string SudokuNummer)
+        public Sudoku(string numbers)
         {
-            this.sudokoNummer = SudokuNummer;
-        }
-        
+            int nextnumber = 0;
+            int counter = 0;
+            double rowlength = Math.Sqrt(board.Length);//Gör roten ur board för att få radlängd
 
-        // =======
-        // METODER
-        // =======
-        public void SkrivUtSudokoBräde()
-        {
-            // Räknare för att få ut korrekt grafik i consolen
-            int countRow = 1;
-            int countCol = 1;
-            int indexOfSudokuArrayValu = 0;
-
-
-            // Två loopar som går igenom alla nummer i sudokon
-            Console.WriteLine("---------------------");
-            for (int col = 0; col < 9; col++)
+            Console.WriteLine(" -------------------");//Grafisk erotik för Sudoku
+            for (int i = 0; i < rowlength; i++)//Här gör vi loop för att få sifforna i rader, kolumner samt i boxar
             {
-
-                // Inre loopen skiver ut varje nummer på en plats
-                for (int row = 0; row < 9; row++)
+                for (int j = 0; j < rowlength; j++)
                 {
-                    char tecken = sudokoNummer[indexOfSudokuArrayValu];
-                    Console.Write(tecken + " ");
-                    if (countRow == 3 || countRow == 6) // if-satsen för visuel effekt
+                    board[i, j] = numbers[nextnumber] - '0'; //Subtrahera med 48 i ascii-tabell (för att undgå string till int fel) 
+                    checknr = numbers[nextnumber].ToString();
+                    row[i] = row[i] + numbers[nextnumber];//Sparar i en rad
+                    col[j] = col[j] + numbers[nextnumber];//Sparar i en kolumn
+
+                    if (i < 3 && j < 3)//Box 1 - Topleft
                     {
-                        Console.Write("| ");
+                        box[0] = box[0] + numbers[nextnumber];
+                    }
+                    else if (i < 3 && j < 6 && j > 2)//Box 2 - Topmid
+                    {
+                        box[1] = box[1] + numbers[nextnumber];
+                    }
+                    else if (i < 3 && j < 9 && j > 5)//Box 3 - Topright
+                    {
+                        box[2] = box[2] + numbers[nextnumber];
+                    }
+                    else if (i < 6 && i > 2 && j < 3)//Box 4 - Midleft
+                    {
+                        box[3] = box[3] + numbers[nextnumber];
+                    }
+                    else if (i < 6 && i > 2 && j < 6 && j > 2)//Box 5 - Midmid
+                    {
+                        box[4] = box[4] + numbers[nextnumber];
+                    }
+                    else if (i < 6 && i > 2 && j < 9 && j > 5)//Box 6 - Midright
+                    {
+                        box[5] = box[5] + numbers[nextnumber];
+                    }
+                    else if (i < 9 && i > 5 && j < 3)//Box 7 - Botleft
+                    {
+                        box[6] = box[6] + numbers[nextnumber];
+                    }
+                    else if (i < 9 && i > 5 && j < 6 && j > 2)//Box 8 - Botmid
+                    {
+                        box[7] = box[7] + numbers[nextnumber];
+                    }
+                    else //Box 9 - Botright
+                    {
+                        box[8] = box[8] + numbers[nextnumber];
                     }
 
-                    // Fyller listan med alla nummer som inte är 0/Tomma platser
-                    if (tecken != '0')
-                    {
-                        nummerSomFinns.Add(tecken);
-                        FyllBoxarna(col, row, tecken);
-                    }
+                    Console.Write(" " + board[i, j]);
 
-                    // Fyller boxarna med värden
-                    // FyllBoxarna(col, row, tecken);
-                    countRow++;
-                    indexOfSudokuArrayValu++; // Går till nästa värde som ska skrivas ut
+                    if ((j + 1) % 3 == 0 && j != rowlength - 1)//Grafisk erotik för Sudoku - sker efter varje 3:e siffra förutom 9
+                    {
+                        Console.Write("|");
+                    }
+                    else { }
+                    counter++;
+                    nextnumber++;
 
                 }
 
                 Console.WriteLine();
-                if (countCol == 3) // if-sats för visuel effekt
+                if ((i + 1) % 3 == 0)//Grafisk erotik för Sudoku
                 {
-                    Console.WriteLine("---------------------");
-                    countCol = 0;
+                    Console.WriteLine(" -------------------");
                 }
+                else { }
 
-                countCol++;
-                countRow = 1;
-                
             }
 
-            
-        }
-
-        // Fyller Boxarna med värden
-        public void FyllBoxarna(int col, int row, char tecken)
-        {
-            // Topp Till vänster
-            if (col < 3 && row < 3)
-            {
-                topLeftBox[box1] = tecken;
-                box1++;
-            }
-            // Toppen i mitten
-            else if (col < 3 && row < 6)
-            {
-                topMideBox[box2] = tecken;
-                box2++;
-            }
-            // Toppen till höger
-            else if (col < 3 && row > 5)
-            {
-                topRightBox[box3] = tecken;
-                box3++;
-            }
-            // Mitten till vänster
-            else if (col < 6 && row < 3)
-            {
-                middleLeftBox[box4] = tecken;
-                box4++;
-            }
-            // Mitten i Mitten
-            else if (col < 6 && row < 6)
-            {
-                middleMidBox[box5] = tecken;
-                box5++;
-            }
-            // Mitten till Höger
-            else if (col < 6 && row > 5)
-            {
-                middleRightBox[box6] = tecken;
-                box6++;
-            }
-            //Botten till vänster
-            else if (col > 5 && row < 3)
-            {
-                bottomLeftBox[box7] = tecken;
-                box7++;
-            }
-            // Botten i Mitten
-            else if (col > 5 && row < 6)
-            {
-                bottomMidBox[box8] = tecken;
-                box8++;
-            }
-            // Botten till Höger
-            else
-            {
-                bottomRightBox[box9] = tecken;
-                box9++;
-            }
+            //for (int k = 0; k < rowlength; k++) // Såhär gör man när man vill söka igenom en rad
+            //{
+            //    Console.Write(board[k, 2]); //Detta är hela kolumn 3
+            //}
 
         }
-
-        // Onödig metod som ska tas bord innan allt är klar 
-        // Skriver endast ut alla boxar och dess nummer
-        public void SkrivUtAllaBoxar()
-        {
-            Console.WriteLine("Top Left Box");
-            foreach (var item in topLeftBox)
-            {
-                Console.Write(item + " ");
-            }
-            Console.WriteLine();
-            Console.WriteLine("Top Mid Box");
-            foreach (var item in topMideBox)
-            {
-                Console.Write(item + " ");
-            }
-            Console.WriteLine();
-            Console.WriteLine("Top Rigt box");
-            foreach (var item in topRightBox)
-            {
-                Console.Write(item + " ");
-            }
-            Console.WriteLine();
-            Console.WriteLine("Mid left box");
-            foreach (var item in middleLeftBox)
-            {
-                Console.Write(item + " ");
-            }
-            Console.WriteLine();
-            Console.WriteLine("Mid mid box");
-            foreach (var item in middleMidBox)
-            {
-                Console.Write(item + " ");
-            }
-            Console.WriteLine();
-            Console.WriteLine("Mid Right Box");
-            foreach (var item in middleRightBox)
-            {
-                Console.Write(item + " ");
-            }
-            Console.WriteLine();
-            Console.WriteLine("Bottom left");
-            foreach (var item in bottomLeftBox)
-            {
-                Console.Write(item + " ");
-            }
-            Console.WriteLine();
-            Console.WriteLine("Bottom middle box");
-            foreach (var item in bottomMidBox)
-            {
-                Console.Write(item + " ");
-            }
-            Console.WriteLine();
-            Console.WriteLine("Bottom riight box");
-            foreach (var item in bottomRightBox)
-            {
-                Console.Write(item + " ");
-            }
-        }
-
-        public void LösSudokun()
-        {
-            // 
-            int idexOfSudokuArrayValu = 0;
-
-            // Två loopar som går igenom alla nummer i sudokon
-            Console.WriteLine();
-            for (int col = 0; col < 9; col++)
-            {
-
-                // Inre loopen skiver ut varje nummer på en plats
-                for (int row = 0; row < 9; row++)
-                {
-                    char nummerICell = sudokoNummer[idexOfSudokuArrayValu];
-
-                    if (nummerSomFinns.Contains(nummerICell))
-                    {
-                        Console.WriteLine("Nummret finns ");
-                    }
-
-                    else if (nummerSomFinns.Contains(nummerICell) == false)
-                    {
-                        Console.WriteLine("nummret fins inte");
-                    }
-
-                    idexOfSudokuArrayValu++; // Går till nästa värde som ska skrivas ut
-
-                }
-
-                Console.ReadLine();
-            }
-        }
-
     }
 }
